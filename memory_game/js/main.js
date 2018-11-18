ab801e6186eaad265a2b7ec6468648a7b0922e7b
SirAbbott <tom@iynk.com>
2018-11-18 19:47:21 +0000

    add reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,43 +22,58 @@ var cards = [
   }
 ];
 
-var cardsInPlay = [];
+var cardsInPlay;
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  function checkMatch() {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!!");
-    } else if (cardsInPlay.length === 2) {
-      alert("Sorry, try again");
-    }
-  }
+  // Setup eventListener for reset
+  reset();
+  // Start game
+  start();
+}
 
-  function flipCard() {
-    var cardId = this.getAttribute("data-id");
-    this.setAttribute("src", cards[cardId].cardImage);
-    // console.log("User flipped" + " " + cards[cardId].rank);
-    // console.log(cards[cardId].cardImage);
-    // console.log(cards[cardId].suit);
-    cardsInPlay.push(cards[cardId].rank);
-    checkMatch();
-  }
-
-  function createBoard() {
-    for (var i = 0; i < cards.length; i++) {
-      // const j = Math.floor(Math.random() * (i + 1));
-      // [cards[i], cards[j]] = [cards[j], cards[i]];
-      var cardElement = document.createElement("img");
-      cardElement.setAttribute("src", "images/back.png");
-      cardElement.setAttribute("data-id", i);
-      cardElement.addEventListener("click", flipCard);
-      document.getElementById("game-board").appendChild(cardElement);
-    }
-  }
-
+function start() {
+  // Ensure always clear variable
+  cardsInPlay = [];
+  // Empty board contents
+  const board = document.getElementById("game-board");
+  board.innerHTML = "";
+  // Create board
   createBoard();
 }
 
-// var resetButton = document.getElementById("reset");
-// resetButton.addEventListener("click", init);
+function checkMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!!");
+  } else if (cardsInPlay.length === 2) {
+    alert("Sorry, try again");
+  }
+}
+
+function flipCard() {
+  var cardId = this.getAttribute("data-id");
+  this.setAttribute("src", cards[cardId].cardImage);
+  // console.log("User flipped" + " " + cards[cardId].rank);
+  // console.log(cards[cardId].cardImage);
+  // console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
+  checkMatch();
+}
+
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    // const j = Math.floor(Math.random() * (i + 1));
+    // [cards[i], cards[j]] = [cards[j], cards[i]];
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+
+function reset() {
+  var resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", start);
+}
